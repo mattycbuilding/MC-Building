ab3d00e0a09743f963b7bfd9d6197381a323463d
mattycbuilding <mattyc.building@gmail.com>
2026-01-29 22:16:44 +1300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE="mcb-cache-v16-20260129193000";
+const CACHE="mcb-cache-v19-20260129211500";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
+  "./sync-config.js",
   "./manifest.webmanifest",
   "./logo.png",
   "./icon-192.png",
